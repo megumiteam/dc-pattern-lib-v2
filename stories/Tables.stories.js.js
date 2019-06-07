b762088a494eb012a2141a7c9b9d2d6f1623b05f
Daniel Olson <emaildano@gmail.com>
2019-06-07 14:26:24 -0400

    added/updated table styles

---
+++ stories/Tables.stories.js.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
+import {Table} from 'reactstrap';
+
+// target
+import DashboardCard from '../src/DashboardCard';
+import '../src/assets/main.css';
+
+storiesOf('Tables', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withInfo)
+  .add('Full Example', () => (
+    <DashboardCard>
+    <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      </DashboardCard>
+  ));
