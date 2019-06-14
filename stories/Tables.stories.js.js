22696e18ee77b4f446d3e1c16e9f609f19a5c50d
Daniel Olson <emaildano@gmail.com>
2019-06-14 14:21:01 -0400

    updated tables and colors

--- stories/Tables.stories.js.js
+++ stories/Tables.stories.js.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
-import {Table} from 'reactstrap';
+import { Table } from 'reactstrap';
 
 // target
 import DashboardCard from '../src/DashboardCard';
@@ -14,7 +14,7 @@ storiesOf('Tables', module)
   .addDecorator(withInfo)
   .add('Full Example', () => (
     <DashboardCard>
-    <Table>
+      <Table>
         <thead>
           <tr>
             <th>#</th>
@@ -44,5 +44,39 @@ storiesOf('Tables', module)
           </tr>
         </tbody>
       </Table>
-      </DashboardCard>
+    </DashboardCard>
+  ))
+  .add('Borderless', () => (
+    <DashboardCard>
+      <Table borderless>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </DashboardCard>
   ));
