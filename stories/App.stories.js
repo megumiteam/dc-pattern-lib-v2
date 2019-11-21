49ecc67f5938b17c88ace7f012259d47de18acf1
Jason <jmcclintok07@gmail.com>
2019-11-21 16:21:32 -0500

    merge

--- stories/App.stories.js
+++ stories/App.stories.js
@@ -2,7 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import AppDimmer from '../src/AppDimmer';
 
-// target
-import '../src/assets/main.css';
-
 storiesOf('App', module).add('Dimmer', () => <AppDimmer isOpen />);
