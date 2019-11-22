26f972f47d066ce8d23c8f98224b56e7414ad449
Jason <jmcclintok07@gmail.com>
2019-11-22 12:24:12 -0500

    updated dashboard sidebar nav size

--- src/DashboardNav.js
+++ src/DashboardNav.js
@@ -5,22 +5,22 @@ import { FaBeer } from 'react-icons/fa';
 const DashboardNav = ({ children }: { children: React.Node }) => (
   <div className="w-100">
     <Nav className="nav-sidebar" tabs vertical>
-      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+      <NavLink className="nav-sidebar__link mt-3 pl-5" href="#">
         <FaBeer className="mr-4" />
         {' '}
 Sites
       </NavLink>
-      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+      <NavLink className="nav-sidebar__link mt-3 pl-5" href="#">
         <FaBeer className="mr-4" />
         {' '}
 Teams
       </NavLink>
-      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+      <NavLink className="nav-sidebar__link mt-3 pl-5" href="#">
         <FaBeer className="mr-4" />
         {' '}
 Account
       </NavLink>
-      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+      <NavLink className="nav-sidebar__link mt-3 pl-5" href="#">
         <FaBeer className="mr-4" />
         {' '}
 Guides
--- src/DashboardSidebar.js
+++ src/DashboardSidebar.js
@@ -5,7 +5,7 @@ import { Col } from 'reactstrap';
 type Props = { children: React.Node };
 
 const DashboardSidebar = ({ children }: Props) => (
-  <Col md="2" className="fixed-top fixed-bottom shadow-sm d-none d-md-block">
+  <Col md="3" className="fixed-top fixed-bottom shadow-sm d-none d-md-block">
     <div className="position-sticky d-flex flex-column align-items-center sidebar__item">
       {children}
     </div>
