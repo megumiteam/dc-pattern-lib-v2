82a498994e458b01a72fb6f4ffca9fb28fe8e320
Jason <jmcclintok07@gmail.com>
2019-11-22 12:17:15 -0500

    updated sidebar nav styling, changed body background color to use gray-light

--- src/DashboardNav.js
+++ src/DashboardNav.js
@@ -3,25 +3,25 @@ import { Nav, NavLink } from 'reactstrap';
 import { FaBeer } from 'react-icons/fa';
 
 const DashboardNav = ({ children }: { children: React.Node }) => (
-  <div>
-    <Nav vertical>
-      <NavLink href="#">
-        <FaBeer />
+  <div className="w-100">
+    <Nav className="nav-sidebar" tabs vertical>
+      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+        <FaBeer className="mr-4" />
         {' '}
 Sites
       </NavLink>
-      <NavLink href="#">
-        <FaBeer />
+      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+        <FaBeer className="mr-4" />
         {' '}
 Teams
       </NavLink>
-      <NavLink href="#">
-        <FaBeer />
+      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+        <FaBeer className="mr-4" />
         {' '}
 Account
       </NavLink>
-      <NavLink href="#">
-        <FaBeer />
+      <NavLink className="nav-sidebar__link mt-2 pl-5" href="#">
+        <FaBeer className="mr-4" />
         {' '}
 Guides
       </NavLink>
--- src/DashboardSidebar.js
+++ src/DashboardSidebar.js
@@ -5,8 +5,10 @@ import { Col } from 'reactstrap';
 type Props = { children: React.Node };
 
 const DashboardSidebar = ({ children }: Props) => (
-  <Col md="3" className="fixed-top fixed-bottom shadow-sm d-none d-md-block">
-    <div className="position-sticky d-flex flex-column align-items-center">{children}</div>
+  <Col md="2" className="fixed-top fixed-bottom shadow-sm d-none d-md-block">
+    <div className="position-sticky d-flex flex-column align-items-center sidebar__item">
+      {children}
+    </div>
   </Col>
 );
 export default DashboardSidebar;
